74bb4c5169e4961edb8ef7a3fe90dadd0d2b55c3
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-13 16:38:35 +0530

    Update dice_rolling_simulator.py

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -1,8 +1,8 @@
 import random
 
 
-min = 1
-max = 6
+min_no = 1
+max_no = 6
 
 N = int(input("enter the number of dice you want to roll?"))
 
@@ -11,8 +11,8 @@ roll_again = "yes"
 while roll_again == "yes" or roll_again == "y":
     print ("Rolling the dices...")
     print ("The values are....")
-    print (random.randint(min, max))
-    print (random.randint(min, max))
+    print (random.randint(min_no, max_no))
+    print (random.randint(min_no, max_no))
 
     roll_again = int(input("Roll the dices again?"))
     
