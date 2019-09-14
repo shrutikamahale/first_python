cf00e837b7799c2d050089c4c291b85ec28a03be
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2019-09-14 21:54:28 +0530

    Add files via upload
    3rd_python project

---
+++ mad_lib.py
@@ -0,0 +1,7 @@
+technology = input("Enter a Technology you wish to explore: ")
+launguage = input("Enter your gateway into technology: ")
+job = input("Enter a future job you wish to apply: ")
+
+print("welcome to the world of " +technology)
+print(launguage  +  " will be your gateway to enter into programming")
+print("You will be "  + job +  " in few months...")
