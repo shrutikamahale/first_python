397b082e0472cfcac304abd1d56a5562418c9fd7
shrutikamahale <36602735+shrutikamahale@users.noreply.github.com>
2020-09-06 17:09:45 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,3 +1,11 @@
-#Python Program to find the length of a String
-name = str(input("Enter a name "))
-print(len(name))
+#Python Program to find factorial of a number
+num = int(input("Enter a number: "))
+factorial = 1
+if (num == 0):
+print("factorial of 0 is 1")
+elif (num < 0):
+print("factorial of negative number does not exist")
+else:
+for i in range (1, num+1):
+factorial=factorial*i
+print("factorial of",num, "is", factorial)
